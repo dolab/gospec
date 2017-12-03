df6213a2c07a17f99a4774991684f8beb9a53c98
Spring MC <Heresy.MC@gmail.com>
2017-12-03 13:54:32 +0800

    What: new brushes
    Why:
    
      * highlight output
    
    How:
    
      * nop

---
+++ brushes.go
@@ -0,0 +1,12 @@
+package gospec
+
+import "github.com/dolab/colorize"
+
+var (
+	gray    = colorize.New("gray")
+	cyan    = colorize.New("cyan")
+	green   = colorize.New("green")
+	blue    = colorize.New("blue")
+	yellow  = colorize.New("yellow")
+	magenta = colorize.New("magenta")
+)

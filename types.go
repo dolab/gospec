a0bf05c45bfedfedceb1444b9c34acbe7f40c638
Spring MC <Heresy.MC@gmail.com>
2017-12-03 13:48:58 +0800

    What: new types for gospec
    Why:
    
      * s-exp like grammer
    
    How:
    
      * nop

--- types.go
+++ types.go
@@ -1,5 +1,9 @@
 package gospec
 
+import (
+	"testing"
+)
+
 const (
 	labelNewLine    = "\n\r\t"
 	labelErrorTrace = "Error Trace"
@@ -15,9 +19,24 @@ type (
 	TestingT interface {
 		Errorf(format string, args ...interface{})
 	}
+
+	// TestingR is the interface that wraps the basic Run method.
+	//
+	// Run runs runner as a subtest of t called name. It reports whether runner succeeded. Run
+	// runs runner in a separate goroutine and will block until all its parallel subtests
+	// have completed.
+	TestingR interface {
+		Run(title string, runner func(t *testing.T)) bool
+	}
 )
 
 type (
+	// S defines a custom func that returns a new grammar with given value
+	S func(actual interface{}) *grammar
+
+	// Expectation defines a custom func that wraps testing examples
+	Expectation func(example string, runner func(expect S))
+
 	// Comparison defines a custom func that returns true on success and false on failure
 	Comparison func() (success bool)
 
